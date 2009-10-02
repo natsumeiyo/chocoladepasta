aab0737129112f8c73fb9b636f0c4ead3b784b86
tamarvansteenbergen <tamarvansteenbergen@d74af65e-af26-11de-b4c2-c9e47ae42fda>
2009-10-02 09:02:44 +0000

    Nieuwe kleurtjes.

--- trunk/Mandelbrot/src/Mandelbrot.java
+++ trunk/Mandelbrot/src/Mandelbrot.java
@@ -8,45 +8,56 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 	int iterations, zoomFactor;
 	TextField xOriginTextField, yOriginTextField, iterationsTextField,
 			scaleFactorTextField, zoomFactorTextField;
+	Choice colorThemeMenu;
+
+	String[] colorThemeStrings = { "Black and White", "Gray", "Red", "Green",
+			"Blue", "Purple", "Yellow", "Rainbow" };
 
 	public void init() {
 
 		xOriginTextField = new TextField("0.0", 10);
-		this.add(xOriginTextField);
+		add(xOriginTextField);
 		xOriginTextField.addActionListener(this);
 		xOrigin = 0.0;
 
 		yOriginTextField = new TextField("0.0", 10);
-		this.add(yOriginTextField);
+		add(yOriginTextField);
 		yOriginTextField.addActionListener(this);
 		yOrigin = 0.0;
 
 		iterationsTextField = new TextField("50", 10);
-		this.add(iterationsTextField);
+		add(iterationsTextField);
 		iterationsTextField.addActionListener(this);
 		iterations = 50;
 
 		scaleFactorTextField = new TextField("0.01", 10);
-		this.add(scaleFactorTextField);
+		add(scaleFactorTextField);
 		scaleFactorTextField.addActionListener(this);
 		scaleFactor = 0.01;
 
 		zoomFactorTextField = new TextField("2", 10);
-		this.add(zoomFactorTextField);
+		add(zoomFactorTextField);
 		zoomFactorTextField.addActionListener(this);
 		zoomFactor = 2;
 
-		this.addMouseListener(this);
+		addMouseListener(this);
+
+		colorThemeMenu = new Choice();
+		add(colorThemeMenu);
+		for (int t = 0; t < colorThemeStrings.length; t++) {
+			colorThemeMenu.add(colorThemeStrings[t]);
+		}
+		colorThemeMenu.select(1);
 
 	}
 
-	private int mandelbrotSet(double x, double y) {
+	private int mandelNumber(double x, double y) {
 
 		int i = 0;
 		double a, aTemp, b;
 		a = 0;
 		b = 0;
-		while ((a * a + b * b) <= 4 && i <= iterations) {
+		while ((a * a + b * b) <= 4 && i < iterations) {
 			aTemp = a * a - b * b + x;
 			b = 2 * a * b + y;
 			a = aTemp;
@@ -58,15 +69,41 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 
 	private Color mandelColor(int ms) {
 
-		int r, b, g;
-		r = b = g = 0;
+		int colorTheme = colorThemeMenu.getSelectedIndex();
 
-		if (ms % 2 == 0) {
-			r = b = g = 255;
-		} else {
-			r = b = g = 0;
+		int r, g, b;
+		r = g = b = 0;
+		double saturation = 0.3;
+
+		/*
+		 * String[] colorThemeStrings = { "Black and White", "Gray", "Red",
+		 * "Green", "Blue", "Purple", "Yellow", "Rainbow" };
+		 */
+
+		switch (colorTheme) {
+		case 0:
+			// black and white
+			if (ms % 2 == 1) {
+				r = g = b = 255;
+			} else {
+				r = g = b = 0;
+			}
+			break;
+		case 1:
+			// gray
+			r = g = b = ms * 255 / iterations;
+			break;
+		case 2:
+			// red
+			r = 255;
+			g = b = 255 - ms * 255 / iterations;
+			break;
+		case 7:
+			return Color.getHSBColor((float) ms / iterations * 2 / 3, 1, 1);
 		}
-		return new Color(r, b, g);
+
+		return new Color(r, g, b);
+
 	}
 
 	private double toMandelX(int xpixel) {
@@ -81,7 +118,7 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 
 		for (int xpixel = 0; xpixel < getWidth(); xpixel++) {
 			for (int ypixel = 0; ypixel < getHeight(); ypixel++) {
-				g.setColor(mandelColor(mandelbrotSet(toMandelX(xpixel),
+				g.setColor(mandelColor(mandelNumber(toMandelX(xpixel),
 						toMandelY(ypixel))));
 				g.fillRect(xpixel, ypixel, 1, 1);
 
@@ -92,6 +129,8 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 	public void mouseClicked(MouseEvent e) {
 
 		if (e.getY() < 50) {
+			// Ugly hack: make sure the applet doesn't zoom in when a text field
+			// is clicked.
 			return;
 		}
 
@@ -109,7 +148,7 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 		scaleFactorTextField.setText("" + scaleFactor);
 		zoomFactorTextField.setText("" + zoomFactor);
 
-		this.repaint();
+		repaint();
 	}
 
 	public void mouseMoved(MouseEvent e) {
@@ -138,7 +177,7 @@ public class Mandelbrot extends Applet implements MouseListener, ActionListener 
 		scaleFactor = Double.parseDouble(scaleFactorTextField.getText());
 		zoomFactor = Integer.parseInt(zoomFactorTextField.getText());
 
-		this.repaint();
+		repaint();
 
 	}
 
