370c4989ba405bdf486a56c7975d33ff9b51abd3
tamarvansteenbergen <tamarvansteenbergen@d74af65e-af26-11de-b4c2-c9e47ae42fda>
2009-10-27 14:03:53 +0000

    Jeroen versie.

--- SchetsPlus/src/Schets.java
+++ SchetsPlus/src/Schets.java
@@ -1,46 +1,45 @@
 import java.awt.Graphics;
-//import java.awt.Color;
-//import java.awt.image.BufferedImage;
-import java.util.LinkedList;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+//import java.util.LinkedList;
 
 class Schets {
-//	private BufferedImage bitmap;
-	private LinkedList<Element> elements;
+	private BufferedImage bitmap;
+//	private LinkedList<Element> elements;
 
 	Schets() {
-		elements = new LinkedList<Element>();
-//		bitmap = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+//		elements = new LinkedList<Element>();
+		bitmap = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 	}
 
 	public void teken(Graphics g) {
-//		g.drawImage(bitmap, 0, 0, null);
+		g.drawImage(bitmap, 0, 0, null);
 		
-		for (int i = elements.size() - 1; i >= 0; i++) {
-			elements.get(i).draw(g);
-		}
+//		for (int i = elements.size() - 1; i >= 0; i++) {
+//			elements.get(i).draw(g);
+//		}
 		
 	}
 
 	public void resize(int w, int h) {
-//		if (w != bitmap.getWidth() || h != bitmap.getHeight()) {
-//			BufferedImage nieuw = new BufferedImage(w, h,
-//					BufferedImage.TYPE_INT_RGB);
-//			Graphics g = nieuw.getGraphics();
-//			g.setColor(Color.WHITE);
-//			g.fillRect(0, 0, w, h);
-//			g.drawImage(bitmap, 0, 0, null);
-//			bitmap = nieuw;
-//		}
+		if (w != bitmap.getWidth() || h != bitmap.getHeight()) {
+			BufferedImage nieuw = new BufferedImage(w, h,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics g = nieuw.getGraphics();
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, w, h);
+			g.drawImage(bitmap, 0, 0, null);
+			bitmap = nieuw;
+		}
 	}
 
 	public Graphics getBitmapGraphics() {
-//		return bitmap.getGraphics();
-		return this.getBitmapGraphics();
+		return bitmap.getGraphics();
 	}
 
 	public void clear() {
-//		Graphics g = bitmap.getGraphics();
-//		g.setColor(Color.WHITE);
-//		g.fillRect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+		Graphics g = bitmap.getGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, bitmap.getWidth(), bitmap.getHeight());
 	}
 }
