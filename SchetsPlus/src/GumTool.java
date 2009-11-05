f03cb5b49cd181fea0af4f20e372d1ba02c04fd0
tamarvansteenbergen <tamarvansteenbergen@d74af65e-af26-11de-b4c2-c9e47ae42fda>
2009-11-05 14:39:24 +0000

    GumTool werkt nu perfect voor de elements die al een isHitBy boolean hebben.

--- SchetsPlus/src/GumTool.java
+++ SchetsPlus/src/GumTool.java
@@ -1,15 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.util.List;
 
-import tamar.schetsplus.elements.*;
+import tamar.schetsplus.elements.Element;
 
 public class GumTool implements Tool {
-	
+
+	private List<Element> el;
+
 	public void muisIngedrukt(SchetsCanv canvas, Point p) {
-		for (Element e : canvas.getSchets().elements) {
-			if (e.hits(p)) {
-				canvas.getSchets().eraseElement(e);
+		el = canvas.getSchets().elements;
+		// traverse the list backwards, so that the most recent element is
+		// removed
+		for (int i = el.size() - 1; i >= 0; i--) {
+			if (el.get(i).isHitBy(p)) {
+				canvas.getSchets().eraseElement(el.get(i));
 				canvas.repaint();
 				return;
 			}
@@ -20,12 +26,12 @@ public class GumTool implements Tool {
 		return null;
 	}
 
-	public void letterIngetikt(SchetsCanv canvas, char c) {	
+	public void letterIngetikt(SchetsCanv canvas, char c) {
 	}
 
 	public void muisVersleept(SchetsCanv canvas, Point p) {
 	}
 
-	public void drawIcon(Graphics2D g, int x, int y, int w, int h) {		
+	public void drawIcon(Graphics2D g, int x, int y, int w, int h) {
 	}
 }
